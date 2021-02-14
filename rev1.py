ea6e189ff1c84ed62dd33482120dea3856d29337
George <georgeyhere@gmail.com>
2021-02-13 20:59:34 -0800

    Update rev1.py

--- rev1.py
+++ rev1.py
@@ -1,12 +1,73 @@
 import numpy
 import math
 
+
+# function to do the transform stuff stuff
+def calculate(axis,degrees,vector_x,vector_y,vector_z):
+      # load array with offset vector and filler data
+      transform_matrix = numpy.array([ [0,0,0,vector_x],[0,0,0,vector_y],[0,0,0,vector_z],[0,0,0,1] ]) 
+      matrix_product = numpy.array([ [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0] ]) 
+      
+      if(axis.lower()=='x'): # x-rotation matrix data
+            transform_matrix[0][0] = 1
+            transform_matrix[0][1] = 0
+            transform_matrix[0][2] = 0
+            
+            transform_matrix[1][0] = 0
+            transform_matrix[1][1] = numpy.cos(degrees_rad)
+            transform_matrix[1][2] = -1*numpy.sin(degrees_rad)
+            
+            transform_matrix[2][0] = 0
+            transform_matrix[2][1] = numpy.sin(degrees_rad)
+            transform_matrix[2][2] = numpy.cos(degrees_rad)
+      
+      if(axis.lower()=='y'): # y-rotation matrix data
+            transform_matrix[0][0] = numpy.cos(degrees_rad)
+            transform_matrix[0][1] = 0
+            transform_matrix[0][2] = numpy.sin(degrees_rad)
+            
+            transform_matrix[1][0] = 0
+            transform_matrix[1][1] = 1
+            transform_matrix[1][2] = 0
+            
+            transform_matrix[2][0] = -1*numpy.sin(degrees_rad)
+            transform_matrix[2][1] = 0
+            transform_matrix[2][2] = numpy.cos(degrees_rad)    
+            
+      if(axis.lower()=='z'): # z-rotation matrix data
+            transform_matrix[0][0] = numpy.cos(degrees_rad)
+            transform_matrix[0][1] = -1*numpy.sin(degrees_rad)
+            transform_matrix[0][2] = 0
+            
+            transform_matrix[1][0] = numpy.sin(degrees_rad)
+            transform_matrix[1][1] = numpy.cos(degrees_rad) 
+            transform_matrix[1][2] = 0
+            
+            transform_matrix[2][0] = 0
+            transform_matrix[2][1] = 0
+            transform_matrix[2][2] = 1 
+      
+      transform_matrix = numpy.array(transform_matrix,dtype=float) # convert array to float
+      inverse_matrix = numpy.linalg.inv(transform_matrix) # invert the matrix
+      
+      numpy.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)}) # set print options to only 3 decimal places
+      
+      # print outputs
+      print('The transform is:')
+      print(transform_matrix)
+      print('The inverse transform is:')
+      print(inverse_matrix)
+      print('The product of the transform and the inverse transform is:')
+      print(numpy.dot(transform_matrix,inverse_matrix)) # product of transform times inverse transform
+            
+            
+
+
 axis = 'x'
 degrees = 0
 vector_x = 0
 vector_y = 0
 vector_z = 0
-
 
 print("Welcome to George's Inverse Transform Calculator!")
 print("This program will calculate a transform and inverse transform given")
@@ -39,58 +100,4 @@ vector_y = input()
 print("5) Please enter the z component of the offset vector and press enter.")
 vector_z = input()
 
-# load array with offset vector and filler data
-transform_matrix = numpy.array([ [0,0,0,vector_x],[0,0,0,vector_y],[0,0,0,vector_z],[0,0,0,1] ]) 
-matrix_product = numpy.array([ [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0] ]) 
-
-if(axis.lower()=='x'): # x-rotation matrix data
-      transform_matrix[0][0] = 1
-      transform_matrix[0][1] = 0
-      transform_matrix[0][2] = 0
-      
-      transform_matrix[1][0] = 0
-      transform_matrix[1][1] = numpy.cos(degrees_rad)
-      transform_matrix[1][2] = -1*numpy.sin(degrees_rad)
-      
-      transform_matrix[2][0] = 0
-      transform_matrix[2][1] = numpy.sin(degrees_rad)
-      transform_matrix[2][2] = numpy.cos(degrees_rad)
-
-if(axis.lower()=='y'): # y-rotation matrix data
-      transform_matrix[0][0] = numpy.cos(degrees_rad)
-      transform_matrix[0][1] = 0
-      transform_matrix[0][2] = numpy.sin(degrees_rad)
-      
-      transform_matrix[1][0] = 0
-      transform_matrix[1][1] = 1
-      transform_matrix[1][2] = 0
-      
-      transform_matrix[2][0] = -1*numpy.sin(degrees_rad)
-      transform_matrix[2][1] = 0
-      transform_matrix[2][2] = numpy.cos(degrees_rad)    
-      
-if(axis.lower()=='z'): # z-rotation matrix data
-      transform_matrix[0][0] = numpy.cos(degrees_rad)
-      transform_matrix[0][1] = -1*numpy.sin(degrees_rad)
-      transform_matrix[0][2] = 0
-      
-      transform_matrix[1][0] = numpy.sin(degrees_rad)
-      transform_matrix[1][1] = numpy.cos(degrees_rad) 
-      transform_matrix[1][2] = 0
-      
-      transform_matrix[2][0] = 0
-      transform_matrix[2][1] = 0
-      transform_matrix[2][2] = 1 
-
-transform_matrix = numpy.array(transform_matrix,dtype=float)
-inverse_matrix = numpy.linalg.inv(transform_matrix)
-
-numpy.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-print('The transform is:')
-print(transform_matrix)
-print('The inverse transform is:')
-print(inverse_matrix)
-print('The product of the transform and the inverse transform is:')
-print(numpy.dot(transform_matrix,inverse_matrix))
-      
-      
+calculate(axis,degrees,vector_x,vector_y,vector_z)
